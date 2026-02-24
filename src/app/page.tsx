647ef1008c57122f5409dfb2552c69a1f3d5b546
Rafael Anjos <anjosrafael59@gmail.com>
2026-02-23 22:50:32 -0300

    Explorando recursos do shadcn e tailwind css.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
+import { Button } from "@/components/ui/button"
+import { Card } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator"
+import { Plus } from 'lucide-react'
+
 const Home = () => {
   return (
-    <div>Rafael</div>
+    <main className="w-full h-screen bg-gray-100 flex justify-center items-center">
+      <Card className="w-lg p-4">
+        <div className="flex gap-2">
+          <Input placeholder="Adicionar tarefa"/>
+          <Button variant="outline"><Plus/>Cadastrar</Button>
+        </div>
+        <Separator/>
+      </Card>
+    </main>
   )
 }
 
